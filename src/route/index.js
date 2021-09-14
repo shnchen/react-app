5774cbe6bf5d5e9684f18f92f4541ff448b8bca3
zhoushuquan <zhousq@fosun.com>
2021-09-14 10:52:46 +0800

    Updates

--- src/route/index.js
+++
@@ -1,8 +0,0 @@
-import Home from '../page/home.jsx';
-import List from '../page/list.jsx';
-
-const routers = [
-    {path:'/home', exact:true, component:Home},
-    {path:'/list', exact:true, component:List},
-];
-export default routers;
